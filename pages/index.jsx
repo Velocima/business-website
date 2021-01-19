8dfdbe3d1b9c5d6ec0ab1576a46c35c15571be17
Velo <hartley.max@outlook.com>
2021-01-19 23:50:55 +0000

    basic file structure updated from boilerplate

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<link rel='icon' href='/favicon.ico' />
+				<title>Bespoke Website Design</title>
+			</Head>
+			<main>
+				<h1>Hello World</h1>
+			</main>
+		</>
+	);
+}
